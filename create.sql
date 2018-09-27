f5ec39b8e8847a5130af6c2654e64a0419fe0849
bitcoineler <35832576+bitcoineler@users.noreply.github.com>
2018-09-27 19:21:59 +0200

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE `transactions` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+
+CREATE TABLE `transactions`.`opreturn` (
+  `txid` VARCHAR(255) NOT NULL,
+  `prefix` VARCHAR(255) NULL,
+  `hash` VARCHAR(255) NULL,
+  `type` VARCHAR(255) NULL,
+  `title` VARCHAR(255) NULL,
+  PRIMARY KEY (`txid`));
