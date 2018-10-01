5c3981f706499c3d8a639343cab16b2f28f33d18
Your Name <you@example.com>
2018-10-01 11:16:35 +0200

    base64 encoding

--- app/blockchain/blockchain_sync.go
+++ app/blockchain/blockchain_sync.go
@@ -2,40 +2,40 @@ package main
 
 import (
 	"database/sql"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"regexp"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// {
-//   "v": 2,
-//   "e": { "out.b1": "hex"  },
-//   "q": {
-//     "find": {
-//       "out.b1": "e901",
-//       "out.b0": {
-//         "op": 106
-//       },
-//       "blk.i": {
-//         "$gte" : 550000
-//       }
-//
-//     },
-//     "project": {
-//       "out.b1": 1,
-//       "out.s2": 1,
-//       "out.s3": 1,
-//       "out.s4": 1,
-//       "tx.h": 1,
-//       "_id": 0
-//     }
-//   }
-// }
+var query = `{
+  "v": 2,
+  "e": { "out.b1": "hex"  },
+  "q": {
+    "find": {
+      "out.b1": "e901",
+      "out.b0": {
+        "op": 106
+      },
+      "blk.i": {
+        "$gte" : 550000
+      }
+
+    },
+    "project": {
+      "out.b1": 1,
+      "out.s2": 1,
+      "out.s3": 1,
+      "out.s4": 1,
+      "tx.h": 1,
+      "_id": 0
+    }
+  }
+}`
 
 type Query struct {
 	Unconfirmed []Transaction `json:"unconfirmed"`
@@ -50,6 +50,8 @@ type Transaction struct {
 type Sub struct {
 	B1 string `json:"b1"`
 	S2 string `json:"s2"`
+	S3 string `json:"s3"`
+	S4 string `json:"s4"`
 }
 
 type Id struct {
@@ -83,7 +85,8 @@ func insertIntoMysql(TxId string, TxB1 string, S2Hash string, S2Type string, S2T
 func main() {
 	var q Query
 	//url encoded query : blocksize greater than 550'000
-	url := "https://bitdb.network/q/ewogICJ2IjogMiwKICAiZSI6IHsgIm91dC5iMSI6ICJoZXgiICB9LAogICJxIjogewogICAgImZpbmQiOiB7IAogICAgICAib3V0LmIxIjogImU5MDEiLAogICAgICAib3V0LmIwIjogewogICAgICAgICJvcCI6IDEwNgogICAgICB9LAogICAgICAiYmxrLmkiOiB7CiAgICAgICAgIiRndGUiIDogNTUwMDAwCiAgICAgIH0KCiAgICB9LAogICAgInByb2plY3QiOiB7CiAgICAgICJvdXQuYjEiOiAxLAogICAgICAib3V0LnMyIjogMSwKICAgICAgIm91dC5zMyI6IDEsCiAgICAgICJvdXQuczQiOiAxLAogICAgICAidHguaCI6IDEsCiAgICAgICJfaWQiOiAwCiAgICB9CiAgfQp9"
+	b64_query := base64.StdEncoding.EncodeToString([]byte(query))
+	url := "https://bitdb.network/q/" + b64_query
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("key", "qz6qzfpttw44eqzqz8t2k26qxswhff79ng40pp2m44")
@@ -99,29 +102,40 @@ func main() {
 	for i := range q.Confirmed {
 		TxId := q.Confirmed[i].Tx.H
 		txOuts := q.Confirmed[i].Out
-		var TxB1 string
-		var TxS2 string
+		var Prefix string
+		var Hash string
+		var Type string
+		var Title string
 		for a := range txOuts {
 			if txOuts[a].B1 == "e901" {
-				TxB1 = txOuts[a].B1
-				TxS2 = txOuts[a].S2
+				Prefix = txOuts[a].B1
+				Hash = txOuts[a].S2
+				Type = txOuts[a].S3
+				Title = txOuts[a].S4
 			}
 		}
-		re := `(\S{20,50})\|(\S{4})\|(.*)`
-		rex := regexp.MustCompile(re)
-		matches := rex.FindAllStringSubmatch(TxS2, -1)
 
-		if len(matches) != 0 {
-			var S2Hash string
-			var S2Type string
-			var S2Title string
-			for _, i := range matches {
-				S2Hash = i[1]
-				S2Type = i[2]
-				S2Title = i[3]
-			}
-			insertIntoMysql(TxId, TxB1, S2Hash, S2Type, S2Title)
+		if len(Prefix) != 0 {
+			fmt.Println(TxId, Prefix, Hash, Type, Title)
+			insertIntoMysql(TxId, Prefix, Hash, Type, Title)
 		}
+
+		// re := `(\S{20,50})\|(\S{4})\|(.*)`
+		// rex := regexp.MustCompile(re)
+		// matches := rex.FindAllStringSubmatch(TxS2, -1)
+
+		// if len(matches) != 0 {
+		// 	var S2Hash string
+		// 	var S2Type string
+		// 	var S2Title string
+		// 	for _, i := range matches {
+		// 		S2Hash = i[1]
+		// 		S2Type = i[2]
+		// 		S2Title = i[3]
+		// 	}
+		// 	fmt.Println(TxId, TxB1, S2Hash, S2Type, S2Title)
+		// 	insertIntoMysql(TxId, TxB1, S2Hash, S2Type, S2Title)
+		// }
 	}
 
 }
