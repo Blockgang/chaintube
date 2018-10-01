f93318289866c0bd60a853987c07cd0e247428f7
Your Name <you@example.com>
2018-10-01 12:20:02 +0200

    update

--- test/create.sql
+++ test/create.sql
@@ -3,8 +3,8 @@ CREATE DATABASE `theca` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0
 
 CREATE TABLE `transactions`.`opreturn` (
   `txid` VARCHAR(64) NOT NULL,
-  `prefix` VARCHAR(255) NULL,
+  `prefix` VARCHAR(4) NULL,
   `hash` VARCHAR(255) NULL,
-  `type` VARCHAR(255) NULL,
+  `type` VARCHAR(5) NULL,
   `title` VARCHAR(255) NULL,
   PRIMARY KEY (`txid`));
