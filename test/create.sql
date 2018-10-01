a38cd9d022c96089e502450d066e547bec9302b7
Your Name <you@example.com>
2018-10-01 12:14:50 +0200

    change db name

--- test/create.sql
+++ test/create.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `transactions` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+CREATE DATABASE `theca` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 
 
 CREATE TABLE `transactions`.`opreturn` (
